0f3304b462e1682a549ff050de03dd94cbe07d60
Maximilian Vorwerk <maximilian.vorwerk@student.htw-berlin.de>
2025-01-19 19:39:27 +0100

    test ursachenanalyse

--- src/process_performance.py
+++ src/process_performance.py
@@ -9,6 +9,12 @@ import pandas as pd
 from openai import OpenAI
 from dotenv import load_dotenv
 
+
+import pm4py
+import xml.etree.ElementTree as ET
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.objects.log.exporter.xes import exporter as xes_exporter
+
 #-----------------------------------------------------------------------------
 # Config
 #-----------------------------------------------------------------------------
@@ -44,46 +50,12 @@ def process_performance(event_logs):
 
 #-----------------------------------------------------------------------------
 
+    event_logs_df = pm4py.convert_to_dataframe(event_logs)
 
-    data_frame = pm4py.convert.convert_to_dataframe(event_logs)
-
-    # Umwandlung des Timestamps in ein Datetime-Format
-    data_frame['timestamp'] = pd.to_datetime(data_frame['timestamp'])
-
-    # Berechnung der Durchlaufzeiten pro case_id
-    durations = data_frame.groupby('case_id')['timestamp'].agg(['min', 'max'])
-    durations['duration'] = (durations['max'] - durations['min']).dt.total_seconds()
-
-    # Zusammenführen der Durchlaufzeiten mit den ursprünglichen Daten
-    data_frame = data_frame.merge(durations[['duration']], left_on='case_id', right_index=True)
-    print(data_frame.head())
-
-    relevant_data = data_frame[['concept:name', 'claim_amount', 'type_of_accident', 'user_type', 'duration']]
-    print(relevant_data.head())
-
-    relevant_data = pd.get_dummies(relevant_data, columns=['type_of_accident', 'user_type'])
-
-    correlations = relevant_data.corr()['duration'].sort_values(ascending=False)
-    print(correlations)
-
-    correlations_dict = correlations.to_dict()
-    print(correlations_dict)
-
-
-    with open("prompts/user_performance_prompt_2.txt", "r", encoding="utf-8") as file:
-        user_performance_prompt_2 = file.read()
-
-    user_performance_prompt_2 = user_performance_prompt_2.replace("<<Process-Variants>>", correlations_dict)
-
-    response = client.chat.completions.create(
-        model=model_name,
-        messages=[
-            {"role": "assistant", "content": result_performance_1},
-            {"role": "user", "content": user_performance_prompt_2}
-        ]
+    duration = pm4py.get_case_duration(
+        event_logs,
+        'case_1',
+        activity_key='concept:name',
+        case_id_key='case:concept:name',
+        timestamp_key='time:timestamp'
     )
-
-    result_performance_2 = response.choices[0].message.content
-
-    with open("output/process_performance_output_2.txt", "w", encoding="utf-8") as file:
-        file.write(result_performance_2)
